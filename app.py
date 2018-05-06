7c4cb6e312c13f4d14ba181e41f6273a006b924e
krot4021 <krot4021@users.noreply.github.com>
2018-05-06 09:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(os.path.join(project_dir, "protocol.db"))
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ['DATABASE_URL']
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
